40887bfdb95db24c0966f4cb664385f80414826c
salvogs <salvogs@live.it>
2021-08-26 17:56:44 +0200

    test3 less expensive

--- test/test3_runClient.sh
+++ test/test3_runClient.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
 while true
-do	
-	i=$RANDOM
+do  
+  i=$RANDOM
 
-	if [[ $((i >= 10 && i <= 20)) -eq 1 ]]; then
-		./bin/client -f $socket -t 0 -w testfile -D test/ejectedDir
-	fi
+  
+  if [[  $((i % 5)) -eq 0 ]]; then
+    ./bin/client -f $socket -t 0 -w testfile -D test/ejectedDir -u $PWD/testfile/bigFile1.bin
+  fi
 
-	if [[ $((i >= 100 && i <= 200)) -eq 1 ]]; then
-		./bin/client -f $socket -t 0 -R n=3 -d test/readDir -r "$PWD/testfile/emptyFile.bin","$PWD/testfile/img.jpg" -d test/readDir -l "$PWD/testfile/bigFile.bin" -c "$PWD/testfile/bigFile.bin"
-	fi
+  ./bin/client -f $socket -t 0 -R n=3 -d test/readDir -r "$PWD/testfile/emptyFile.bin","$PWD/testfile/img.jpg" -d test/readDir -l "$PWD/testfile/bigFile.bin" -c "$PWD/testfile/bigFile.bin"
+  
 
-	if [[ $((i >= 1000 && i <=2000)) -eq 1 ]]; then
-		./bin/client -f $socket -t 0 -l "$PWD/testFile/bigFile1.bin" -c "$PWD/testFile/bigFile1.bin" -W testfile/bigFile2.bin,testfile/1KB.bin -u "$PWD/testfile/bigFile2.bin"
-	fi
-
-	if [[ $((i >= 10000 && i <=11000)) -eq 1 ]]; then
-		./bin/client -f $socket -t 0 -w testfile/10byte.txt,testfile/50byte.txt -c "$PWD/testFile/10byte.txt","$PWD/testFile/50byte.txt"
-	fi
-
-	
+  ./bin/client -f $socket -t 0 -w testfile/10byte.txt,testfile/50byte.txt -c "$PWD/testFile/10byte.txt","$PWD/testFile/50byte.txt","$PWD/testfile/bigFile.bin"
+  
+  sleep 0.2
+  
 done
